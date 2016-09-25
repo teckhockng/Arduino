d369bb54cb58251c690cd28e27bb3ef4166d20f4
teckhockng <t.hock.ng@gmail.com>
2016-09-24 21:13:39 -0400

    Add files via upload

---
+++ LightSensor2.ino/LightSensor2.ino.ino
@@ -0,0 +1,36 @@
+int sensorPin = A1;
+int rgbRed = 9;
+int rgbGreen = 10;
+int rgbBlue = 11;
+int sensorValue = 0;
+int bright = 800;
+int moderate = 550;
+int dark = 150;
+
+void setup() {
+  // put your setup code here, to run once:
+ pinMode(rgbRed, OUTPUT);
+  pinMode(rgbGreen, OUTPUT);
+  pinMode(rgbBlue, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+sensorValue = analogRead(sensorPin);
+if ( sensorValue < dark){
+  digitalWrite(rgbRed, HIGH);
+  digitalWrite(rgbBlue, LOW);
+  digitalWrite(rgbGreen, LOW);
+}  
+if ((sensorValue > dark) && (sensorValue < bright)){
+  digitalWrite(rgbBlue, HIGH);
+  digitalWrite(rgbRed, LOW);
+  digitalWrite(rgbGreen, LOW);
+}  
+if  (sensorValue > bright){
+  digitalWrite(rgbGreen, HIGH);
+  digitalWrite(rgbRed , LOW);
+  digitalWrite(rgbBlue, LOW);
+}
+}
+
---
+++ Lvl3Light.ino
@@ -0,0 +1,24 @@
+int sensorPin = A1;// select the input pin for the potentiometer
+int ledPin1 = 13;
+int ledPin2 = 12; // select the pin for the LED
+int sensorValue = 0;  // variable to store the value coming from the sensor
+
+void setup() {
+pinMode(ledPin2, OUTPUT);
+pinMode(ledPin1, OUTPUT);  
+
+}
+
+void loop() {
+ sensorValue = analogRead(sensorPin);
+ if (sensorValue < 400){ 
+  digitalWrite(ledPin1, HIGH);
+  digitalWrite(ledPin2, HIGH); 
+}
+
+ if (sensorValue > 400){ 
+  digitalWrite(ledPin1, LOW);
+  digitalWrite(ledPin2, LOW); 
+}                            
+ 
+}
